bd6196e0a706669eba895da3ab2f325343928479
LuisGonzalez <luis.rigoma@gmail.com>
2017-05-29 11:47:30 -0600

    Agregando proyecto al repositorio
    Ya se incluye la plantilla ColorAdmin

---
+++ CRIPCO/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CRIPCO.Startup))]
+namespace CRIPCO
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
