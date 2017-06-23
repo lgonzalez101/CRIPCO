907a878a6c3531cdec463f8b8326f1b0ac61ee6d
Jakii <jakii_paz.07@hotmail.es>
2017-06-22 21:19:48 -0600

    Modulo de Citas

---
+++ CRIPCO/Models/Citas/ListaEspecialidadesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRIPCO.Models.Citas
+{
+    public class ListaEspecialidadesViewModel
+    {
+        public int EspecialidadID { get; set; }
+        public string Nombre { get; set; }
+        public bool Activo { get; set; }
+    }
+}
