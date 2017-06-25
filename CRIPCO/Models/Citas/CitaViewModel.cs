705362bfb5eed52050d87d2e4ac866c5b5dc8a39
Jakii <jakii_paz.07@hotmail.es>
2017-06-25 01:27:37 -0600

    Vista perfil de usuario

---
+++ CRIPCO/Models/Citas/CitaViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRIPCO.Models.Citas
+{
+    public class CitaViewModel
+    {
+        public int CitaID { get; set; }
+        public int HorarioID { get; set; }
+        public int PersonaPacienteID { get; set; }
+        public string CreadoPor { get; set; }
+        public System.DateTime FechaCreado { get; set; }
+        public string ModificadoPor { get; set; }
+        public bool Activo { get; set; }
+    }
+}
---
+++ CRIPCO/Models/Usuario/DetalleUsuarioViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRIPCO.Models.Usuario
+{
+    public class DetalleUsuarioViewModel
+    {
+        public int PersonaID { get; set; }
+        public string IdAspnetUser { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Identidad { get; set; }
+        public System.DateTime FechaNac { get; set; }
+        public string Telefono { get; set; }
+        public bool Activo { get; set; }
+    }
+}
